0634b8d5c963535a6f7eba34bc1fc673c8f82038
Julia Lober <julia.lober@outlook.com>
2026-09-10 12:15:10 -0600

    combine steps to minimize memory allocation and speed up

--- uavsar_pytools/incidence_angle.py
+++ uavsar_pytools/incidence_angle.py
@@ -10,6 +10,7 @@ def calc_inc_angle(dem, lkv_x, lkv_y, lkv_z, pixel_size=5.556):
     dem, lkv_x, lkv_y, lkv_z : np.array or str
         Elevation data and the three components of the look vector.
         Strings are treated as filepaths to be handled by rasterio.
+        Look vector should be pointing radar-to-ground. 
     pixel_size : float
         Pixel size of all components in [m]. Default value is for 
         UAVSAR images from JPL.
@@ -22,7 +23,7 @@ def calc_inc_angle(dem, lkv_x, lkv_y, lkv_z, pixel_size=5.556):
     # Calculate gradient of DEM
     if type(dem) == str:
         with rio.open(dem) as src:
-            dem_arr = src.read(1).astype(np.float32)
+            dem_arr = src.read(1, out_dtype=np.float32)
             row_grad, col_grad = np.gradient(dem_arr, pixel_size)
             dem_shape = dem_arr.shape
     elif type(dem) == np.ndarray:
@@ -35,13 +36,10 @@ def calc_inc_angle(dem, lkv_x, lkv_y, lkv_z, pixel_size=5.556):
     dx = -col_grad
     dy = row_grad  
 
-    # Calculate unit surface normal vectors: (-dx, -dy, 1)
+    # Calculate magnitude of surface normal vector (-dx, -dy, 1)
     norm_mag = np.sqrt(dx**2 + dy**2 + 1.0)
-    n_x = -dx / norm_mag
-    n_y = -dy / norm_mag
-    n_z = 1.0 / norm_mag
 
-    # Look vectors
+    # Read look vectors
     lkv = {}
     components = [lkv_x, lkv_y, lkv_z]
     directions = ['x','y','z']
@@ -49,7 +47,7 @@ def calc_inc_angle(dem, lkv_x, lkv_y, lkv_z, pixel_size=5.556):
     for comp_idx, vector in enumerate(components):
         if type(vector) == str:
             with rio.open(vector) as src:
-                lkv[directions[comp_idx]] = src.read(1)
+                lkv[directions[comp_idx]] = src.read(1, out_dtype=np.float32)
         elif type(vector) == np.ndarray:
             assert vector.shape == dem_shape, 'Look vector data must be the same shape as DEM data.'
             lkv[directions[comp_idx]] = vector
@@ -60,17 +58,12 @@ def calc_inc_angle(dem, lkv_x, lkv_y, lkv_z, pixel_size=5.556):
     lkv_mag = np.sqrt(lkv['x']**2 + lkv['y']**2 + lkv['z']**2)
     lkv_mag[lkv_mag == 0] = np.nan
 
-    unit_lkv = {}
-    for direction in directions:
-        unit_lkv[direction] = -(lkv[direction] / lkv_mag)
+    # Negatives for dx and dy come from the definition of the surface normal vector 
+    # Negatives for the lkv come from the radar-to-ground default direction
+    dot_product = (-dx * -lkv['x']) + (-dy * -lkv['y']) + (1.0 * -lkv['z'])
 
-    # Calculate Incidence Angle (Dot Product)
-    # Since both vectors are normalized, dot product = cos(theta)
-    inc_cos = (unit_lkv['x'] * n_x) + (unit_lkv['y'] * n_y) + (unit_lkv['z'] * n_z)
-    
-    # Clip to exactly [-1.0, 1.0] to prevent floating point errors from crashing arccos
-    inc_cos = np.clip(inc_cos, -1.0, 1.0)
-    inc = np.arccos(inc_cos)
-
-    return np.rad2deg(inc)
+    # Normalize the dot product with the magnitudes to get cos(inc_angle)
+    # Clipping avoids floating point errors 
+    inc_cos = np.clip(dot_product / (norm_mag * lkv_mag), -1.0, 1.0)
+    return np.rad2deg(np.arccos(inc_cos))
     
